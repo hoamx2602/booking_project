04a53f65375f2026448a37854d46b3185b794b90
hoamx <hoamx.work@gmail.com>
2025-01-18 09:22:31 +0700

    Update title

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { ClerkProvider } from '@clerk/nextjs';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'HomeAway Draft',
+  title: 'Hoang An Hotel',
   description: 'Feel at home, away from home.',
 };
 
